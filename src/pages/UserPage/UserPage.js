4be6ed19b13395aa9a372057e07075e35385eea2
Jandson Costa <jandsoncosta55@gmail.com>
2022-09-15 19:57:35 -0300

    userpage1

--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -1,14 +1,14 @@
-
-import Header from '../../components/Header/Header';
 import './UserPage.css' 
 
-function UserPage() {
+
+function UserPage(props) {
+    console.log('props',props)
     return (
         <div>
          <p>USERPAGE</p>
-         <h1>READ</h1>
-         <h2>READING</h2>
-         <h3>I WANT TO READ</h3>
+         <h1>READ <input type="button"></input></h1>
+         <h2><input type="button"></input>READING <input type="button"></input></h2>
+         <h3><input type="button"></input>I WANT TO READ </h3>
         </div>
     );
 };
