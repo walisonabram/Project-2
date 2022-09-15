d294dbca7615cd660896dd81bb08aed3bb328959
Walison Abram <abramwhas@gmail.com>
2022-09-15 15:17:47 -0300

    home done

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Header from "../../components/Header/Header";
 import Navbar from "../../components/Navbar/Navbar";
 import bookImg from '../../assets/images/logo-home2.png'
@@ -5,18 +6,21 @@ import './Home.css'
 
 function Home() {
   return (
-    <div>
+    <div className="home-container">
       <Header />
       <Navbar />
       <div className="information">
         <h1>Welcome</h1>
-        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim consectetur ullamcorper. Sed sollicitudin facilisis nisi, eu fermentum quam sodales ut. Nam id vestibulum augue.
+        <span>
+          Hello Reader! I know how hard it is to get organized when you're a book lover, here you'll be able to add books you've read, books you're reading and books you'd like to read!
         </span>
         <div className="book">
             <img className="img-book" src={bookImg} alt="book-img" />
         </div>
         <div className="btn">
-            <button>Let's Start</button>
+            <Link to='/register'>
+              <button>Let's Start</button>
+            </Link>
         </div>
       </div>
     </div>
