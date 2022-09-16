494ceb902fd1fccdabca0c0321bb7122908d93bb
Jandson Costa <jandsoncosta55@gmail.com>
2022-09-15 21:18:53 -0300

    read-readin-wreading

--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -1,18 +1,23 @@
-import './UserPage.css' 
+import "./UserPage.css";
 
+function UserPage() {
+  return (
+    <div className="userPage">
+      <h1>USERPAGE</h1>
+      <h3>READ</h3>
+      <div className="book-list">
 
-function UserPage(props) {
-    console.log('props',props)
-    return (
-        <div>
-         <p>USERPAGE</p>
-         <h1>READ <input type="button"></input></h1>
-         <h2><input type="button"></input>READING <input type="button"></input></h2>
-         <h3><input type="button"></input>I WANT TO READ </h3>
-        </div>
-    );
-};
+      </div>
+      <h3>READING</h3>
+      <div className="book-list">
 
-
+      </div>
+      <h3>WANT TO READ</h3>
+      <div className="book-list">
+        
+      </div>
+    </div>
+  );
+}
 
 export default UserPage;
